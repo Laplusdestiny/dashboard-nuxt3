69451a988ca8e61794096b1efed075635ca5f486
Laplusdestiny <prayonshootingstars@gmail.com>
2023-12-13 09:39:23 +0000

    Added SpeedInsights processes in default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,7 @@
     <v-app>
         <AppHeader />
         <slot />
+        <SpeedInsights />
         <AppFooter />
     </v-app>
 </template>
@@ -9,4 +10,5 @@
 <script setup>
 import AppHeader from '~/components/AppHeader.vue'
 import AppFooter from '~/components/AppFooter.vue'
+import { SpeedInsights } from '@vercel/speed-insights/vue'
 </script>
