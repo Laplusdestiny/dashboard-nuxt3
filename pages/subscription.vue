2cfeda0e1e9f24965e5737ca49c5827a02e778a9
Laplusdestiny <prayonshootingstars@gmail.com>
2023-12-13 08:08:33 +0000

    Updated - Added some pages - Updated Readme - Uploaded images

---
+++ pages/subscription.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container class="fill-height">
+        <v-row align="center" justify="center" class="fill-height">
+            <v-col align="center">
+                <!-- <v-img src="your-image-url.jpg" alt="Image" contain></v-img> -->
+                <!-- <v-divider></v-divider> -->
+                <v-row justify="center">
+                    <v-col>
+                        <v-typography align="center">Under construction...</v-typography>
+                    </v-col>
+                </v-row>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<style scoped>
+.fill-height {
+    height: 100%;
+}
+</style>
